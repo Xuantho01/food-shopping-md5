cf845133b94e482f8466c0523ca0707da173e3f2
xuantho <hoxuantho95@gmail.com>
2020-06-14 09:53:22 +0700

    add view admin - add, update, oder-list, tsak

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddComponent } from './add/add.component';
+import { TaskComponent } from './task/task.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { UpdateComponent } from './update/update.component';
+import { OderListComponent } from './oder-list/oder-list.component';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from '../user/layout/layout.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
-
+const routes: Routes = [
+  {path: '',
+    component: LayoutComponent,
+    children: [
+      {path: 'add',
+        component: AddComponent},
+      {path: 'oder-list',
+        component: OderListComponent},
+      {path: 'product-list',
+        component: ProductListComponent},
+      {path: 'user-list',
+        component: ProductListComponent},
+      {path: 'update',
+        component: ProductListComponent}
+    ]
+  },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [AddComponent, TaskComponent, ProductListComponent, UpdateComponent, OderListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
